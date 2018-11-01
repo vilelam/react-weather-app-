f617cbf4556782761760f2f5d60007ce25ff966a
Marcos Vilela <marcosvilela@Marcoss-MacBook-Pro.local>
2018-11-01 18:02:40 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/titles";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
-class App extends Component {
+const API_KEY = "d9ad65140da5d3423345e9fee8b4b5c8";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+    );
+
+    const data = await api_call.json();
+
+    if(city && country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+  
+      console.log(data);
+    } else{
+      this.setState(
+
+        {
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: 'please enter a value'
+        }
+      );
+    }
+
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error = {this.state.error}
+        />
       </div>
     );
   }
---
+++ src/components/form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Form = props => {
+  return (
+    <div>
+      <form onSubmit={props.getWeather}>
+        <input type="text" name="city" placeholder="city..." />
+        <input type="text" name="country" placeholder="country..." />
+        <button>Get Weather</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
